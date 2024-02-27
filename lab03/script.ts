e73aa800a08baa1874392845a6bc5fcfca50ab79
Vanya Slesarev <119366827+shashlyk55@users.noreply.github.com>
2024-02-27 22:57:11 +0300

    Add files via upload

--- lab03/script.ts
+++ lab03/script.ts
@@ -1,18 +1,11 @@
 
 // task 1
-/*
-Напишите функцию, которая принимает массив из 10 целых чисел (от 0 до 9) и
-возвращает строку этих чисел в виде номера телефона.
-Формат номера телефона должен соответствовать "(xxx) xxx-xxxx".
-Пример:
- createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
-// => returns "(123) 456-7890"
- */
 console.log("task 1")
-let createPhoneNumber = (arr:number[]):String => {
+
+let createPhoneNumber = (arr:number[]):String|number => {
     if(arr.length != 10){
         console.log("Массив не содержит 10 элементов!");
-        return "";
+        return 0;
     }
     let str:String = "(";
     for(let i = 0; i < arr.length; i++) {
@@ -29,19 +22,8 @@ let createPhoneNumber = (arr:number[]):String => {
 console.log(createPhoneNumber([1,2,3,4,5,6,7,8,9,0]));
 
 // task 2
-/*
-Если перечислить все натуральные числа до 10, кратные 3 или 5,
-то получим 3, 5, 6 и 9. Сумма этих чисел равна 23.
-Завершите метод так, чтобы он возвращал сумму всех чисел, кратных 3 или 5,
-меньше переданного числа. Кроме того, если число отрицательное, верните 0.
-Примечание. Если число кратно и 3, и 5, считайте его только один раз.
- class Challenge {
- static solution(number: number) {
-    //...
-   }
- }
- */
-console.log("task 2")
+console.log("\ntask 2")
+
 class Challenge {
     static solution(number: number) : number {
         let sum:number = 0;
@@ -57,16 +39,8 @@ class Challenge {
 console.log(Challenge.solution(16));
 
 // task 3
-/*
-Дан целочисленный массив nums, поверните массив вправо на k шагов, где k неотрицательно.
- Пример:
- Ввод: nums = [1,2,3,4,5,6,7], k = 3
- Вывод: [5,6,7,1,2,3,4]
- Объяснение:
- повернуть на 1 шаг вправо: [7,1,2,3,4,5,6]
- повернуть на 2 шага вправо: [6,7,1,2,3,4,5]
- повернуть на 3 шага вправо: [5,6,7,1,2,3,4]
- */
+console.log("\ntask 3")
+
 let arr:number[] = [1,2,3,4,5,6,7];
 let k:number = 3;
 let sliced = arr.splice(arr.length - k, arr.length)
@@ -74,19 +48,8 @@ let res = [...sliced,...arr]
 console.log(res + "")
 
 // task 4
-/*
-Есть два отсортированных массива nums1 и nums2 размера m и n соответственно,
-вернуть медиану двух отсортированных массивов. Медиана число (два числа) находящееся в середине массива.
- Пример 1:
- Ввод: nums1 = [1,3], nums2 = [2]
- Вывод: 2.00000
- Объяснение:
- объединение массивов = [1,2,3], медиана - 2.
- Пример 2:
- Ввод: nums1 = [1,2], nums2 = [3,4]
- Вывод: 2.50000
- Объяснение: объединение массивов = [1,2,3,4], медиана (2 + 3) / 2 = 2.5.
- */
+console.log("\ntask 4")
+
 let nums1:number[] = [1,2]
 let nums2:number[] = [3,4]
 
@@ -99,10 +62,3 @@ else
     mediana = nums[--nums.length / 2]
 
 console.log(mediana)
-
-
-
-
-
-
-
